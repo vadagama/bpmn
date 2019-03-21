9cb59fe0ed5511a2295471a9e16c3e0a8ef1f349
Oleg Krasnov <oleg@vadagama.ru>
2019-03-21 12:14:48 +0300

    New commit

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ var app = new Vue({
     product: 'Socks',
     description: 'A pair of warm fuzzy socks',
     img: 'https://img.znak.com/1904493.jpg',
-    inStock: true,
+    inventory: 12,
+    details: ["80% хлопка", "20% полиэстра", "5% шерсти"],
+    variants: [
+      { 
+        variantId: 101,
+        variantColor: "red",
+      },
+      {
+        variantId: 102,
+        variantColor: "green",
+      }
+    ],
   } 
 })
