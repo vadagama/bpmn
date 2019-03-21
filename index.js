4c8fa58c9f223a7a2688b67bd5edad81a9f71936
Oleg Krasnov <oleg@vadagama.ru>
2019-03-22 00:13:26 +0300

    get this sheet!

--- index.js
+++ index.js
@@ -3,18 +3,33 @@ var app = new Vue({
   data: {
     product: 'Socks',
     description: 'A pair of warm fuzzy socks',
-    img: 'https://img.znak.com/1904493.jpg',
+    image: 'https://weareyoung.ru/wa-data/public/shop/products/87/03/387/images/1645/1645.500.jpg',
     inventory: 12,
     details: ["80% хлопка", "20% полиэстра", "5% шерсти"],
-    variants: [
-      { 
+    variants: [{
         variantId: 101,
-        variantColor: "red",
+        variantColor: "lightgrey",
+        variantImage: "https://weareyoung.ru/wa-data/public/shop/products/87/03/387/images/1645/1645.500.jpg",
       },
       {
         variantId: 102,
-        variantColor: "green",
+        variantColor: "orange",
+        variantImage: "https://weareyoung.ru/wa-data/public/shop/products/87/03/387/images/1648/1648.500.jpg",
       }
     ],
-  } 
+    cart: 0,
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    removeFromCart() {
+      if (this.cart >= 1) {
+        this.cart -= 1
+      }
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage
+    }
+  },
 })
