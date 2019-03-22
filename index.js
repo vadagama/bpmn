58827353592eda03f891583630646f4603d0c2fa
Oleg Krasnov <oleg@vadagama.ru>
2019-03-22 14:46:48 +0300

     Сделал новые расчитываемые значения

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 var app = new Vue({
   el: '#app',
   data: {
-    product: 'Socks',
-    description: 'A pair of warm fuzzy socks',
-    image: 'https://weareyoung.ru/wa-data/public/shop/products/87/03/387/images/1645/1645.500.jpg',
+    brand: "KFC",
+    product: 'Носки',
+    description: 'Пара обалденных шерстянных носков',
+    selectedVariant: 0,
     inventory: 12,
     details: ["80% хлопка", "20% полиэстра", "5% шерсти"],
     variants: [{
         variantId: 101,
         variantColor: "lightgrey",
+        variantColorTitle: "Белые",
+        variantQuantity: 10,
         variantImage: "https://weareyoung.ru/wa-data/public/shop/products/87/03/387/images/1645/1645.500.jpg",
       },
       {
         variantId: 102,
         variantColor: "orange",
+        variantColorTitle: "Желтые",
+        variantQuantity: 5,
         variantImage: "https://weareyoung.ru/wa-data/public/shop/products/87/03/387/images/1648/1648.500.jpg",
       }
     ],
@@ -28,8 +33,19 @@ var app = new Vue({
         this.cart -= 1
       }
     },
-    updateProduct(variantImage) {
-      this.image = variantImage
+    updateProduct(index) {
+      this.selectedVariant = index
     }
   },
+  computed: {
+    title() {
+      return this.brand + " " + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity
+    }
+  }
 })
